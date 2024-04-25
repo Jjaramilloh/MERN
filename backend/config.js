1a8d7b719c27bf15a8d241423f500dbeae7066b1
Jorge Jaramillo <jorgealveiro49@gmail.com>
2024-04-25 16:57:14 -0500

    corrigiendo archivo gitignore

--- backend/config.js
+++ backend/config.js
@@ -1,5 +1,5 @@
 export const PORT = 5555
 
 export const mongoDBURL = 
-//'mongodb+srv://jorgealveiro49:zwBEEetp3VQJF2SA@cluster0.1ewqcty.mongodb.net/?retryWrites=true&w=majority'
+//'mongodb+srv://jorgealveiro49:<password>@cluster0.1ewqcty.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 'mongodb+srv://jorgealveiro49:zwBEEetp3VQJF2SA@cluster0.1ewqcty.mongodb.net/'
