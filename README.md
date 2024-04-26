a9bc6ac5b198515fc0b014d8f492d7b9f55482d6
Jorge Jaramillo <jorgealveiro49@gmail.com>
2024-04-26 17:25:13 -0500

    Creacion de models y rutas para agregar libro a la BD

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # MERN
 Aplicacion utilizando Mongo, Express.js, React.js, Node.js
+
+-Se crea el repositorio local y el de github
+-Creacion de las carpetas backend y frontend 
+-Creacion del archivo inicial (index.js) (config.js) --> contantes
+-Creacion de rutas
+-coneccion base de datos MongoDB habilitacion de IP en mongo cloud
--- backend/config.js
+++ backend/config.js
@@ -1,5 +1,4 @@
 export const PORT = 5555
 
 export const mongoDBURL = 
-//'mongodb+srv://jorgealveiro49:<password>@cluster0.1ewqcty.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
-'mongodb+srv://jorgealveiro49:zwBEEetp3VQJF2SA@cluster0.1ewqcty.mongodb.net/'
+'mongodb+srv://jorgealveiro49:zwBEEetp3VQJF2SA@cluster0.1ewqcty.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
