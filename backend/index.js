bb122d33ba819f89405db05f4a88bdf2bcd321a1
Jorge Jaramillo <jorgealveiro49@gmail.com>
2024-04-28 23:06:22 -0500

    creacion de la carpeta fronted (react tailwind vite )

--- backend/index.js
+++ backend/index.js
@@ -1,63 +1,33 @@
-import express from "express"
+import express, { request, response } from "express"
 import {PORT,mongoDBURL} from "./config.js"
 import mongoose from 'mongoose'
 import { Book } from "./models/bookModel.js"
+import booksRoute from "./routes/booksRoute.js"
+import cors from 'cors'
 
 const app = express()
 
 //Middleware for parsing request body
 app.use(express.json())
 
+//Middleware for handdler CORS POLICY
+//Option 1: Allow All origin with default of cors(*)
+app.use(cors())
+//Option 2: Allow custom origin
+// app.use(
+//     cors({
+//         origin:'http://localhost:3000',
+//         methods:['POST','GET','PUT','DELETE'],
+//         allowedHeaders:['Content-Type'],
+// })
+// )
+
 app.get('/', (request, response) => {
     console.log(request)
     return response.status(234).send(`welcome to MERN Stack Tutorial`)
 })
 
-//Ruta para guardar nuevo libro
-app.post('/books', async(request,response) =>{
-    try {
-        if(
-            !request.body.title ||
-            !request.body.author ||
-            !request.body.publishYear
-        ) {
-            return response.status(400).send({
-                message: 'Send all required fields: title, author, publishYear',
-            });
-        }
-
-        const newBook = {
-            title: request.body.title,
-            author: request.body.author,
-            publishYear: request.body.publishYear,
-        };
-
-        const book =  new Book(newBook)
-        book.save()        
-
-        return response.status(201).send(book)
-        
-    } catch (error) {
-        console.log(error.message)
-        response.status(500).send({message: error.message})
-    }
-})
-
-//Ruta para obtener todos los libros de la base de datos
-app.get('/books', async(request,response)=>{
-    try {
-        const books = await Book.find({})
-
-        return response.status(200).json({
-            count: books.length,
-            data: books
-        })
-
-    } catch (error) {
-        console.log(error.message)
-        response.status(500).send({message: error.message})
-    }
-})
+app.use('/books', booksRoute)
 
 
 mongoose
