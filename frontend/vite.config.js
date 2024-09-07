4a18a180c1d2204440b86f0a076ae0af9833d8db
jjaramilloh <jorgealveiro49@gmail.com>
2024-09-07 17:57:20 -0500

    Se adiciona cambios en la configuracion de vite y los Dockerfile

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,  // Permite que Vite escuche en todas las interfaces de red
+    port: 3000,
+  },
+  preview: {
+    port: 3000, // Cambia a 3000 o el puerto que prefieras
+  }
 })
